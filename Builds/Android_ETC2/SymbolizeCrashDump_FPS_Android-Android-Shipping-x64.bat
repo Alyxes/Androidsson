bdc9b76f7970cba3788787a726f89b77eb8f028a
Timothy <timlennryd@yahoo.se>
2025-06-07 04:00:26 +0200

    meck

---
+++ Builds/Android_ETC2/SymbolizeCrashDump_FPS_Android-Android-Shipping-x64.bat
@@ -0,0 +1,16 @@
+@echo off
+IF %1.==. GOTO NoArgs
+setlocal
+set NDK_ROOT=%ANDROID_NDK_ROOT%
+if "%ANDROID_NDK_ROOT%"=="" set NDK_ROOT=""
+set NDKSTACK=%NDK_ROOT%\ndk-stack.cmd
+
+%NDKSTACK% -sym FPS_Android_Symbols_v1/FPS_Androidx64 -dump "%1" > FPS_Android_SymbolizedCallStackOutput.txt
+
+goto:eof
+
+
+:NoArgs
+echo.
+echo Required argument missing, pass a dump of adb crash log. (SymboliseCallStackDump C:\adbcrashlog.txt)
+pause
